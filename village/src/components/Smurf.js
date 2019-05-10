d6de9e575a93180ad487aa17902cc1a9efefe567
Robert Driskell <driskell.ba@gmail.com>
2019-05-10 12:52:05 -0500

    My last fix introduced more bugs, that I have now fixed

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -5,16 +5,17 @@ import './styles/Smurf.scss';
 const Smurf = props => {
   function deleteSmurf() {
     props.deleteSmurf(props.id)
-    props.history.push('/');
   }
   function setSmurf() {
     props.setSmurf(props.smurf)
   }
   return (
     <div className="Smurf" onClick={setSmurf}>
-      <div className="delete-button">
-        <button onClick={deleteSmurf}>X</button>
-      </div>
+      <Link exact to="/">
+        <div className="delete-button">
+          <button onClick={deleteSmurf}>X</button>
+        </div>
+      </Link>
       <div className="smurf-stats">
         <h3>{props.name}</h3>
         <strong>{props.height} tall</strong>
